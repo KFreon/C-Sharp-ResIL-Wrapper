e44c28f6584d96ea8a7b6c5bb6b998ccf9a3ff82
Freon <kfreon@hotmail.com>
2015-07-22 03:14:25 +0000

    Updated IResILImage.cs

--- ResILWrapper/IResILImage.cs
+++ ResILWrapper/IResILImage.cs
@@ -1,4 +1,4 @@
-public interface IResILImage : IDisposable
+public abstract class ResILImageBase : IDisposable
 {
 	public enum MipMapMode
     {
@@ -21,5 +21,55 @@ public interface IResILImage : IDisposable
 	bool ConvertAndSave(ImageType type, MemoryTributary stream, MipMapMode MipsMode = MipMapMode.BuildAll, CompressedDataFormat surface =CompressedDataFormat.None, int quality = 80, bool SetJPGQuality = true);
 	bool Resize(int width, int height);
 	
-	Dispose();
+	public void Dispose()
+	{
+		
+	}
+	
+	
+	#region Creation
+	public static ResILImageBase Create(string filepath)
+	{
+		using (FileStream stream = new FileStream(filepath))
+			return Create(stream);
+	}
+	
+	public static ResILImageBase Create(MemoryTributary stream)
+	{
+		bool isv8u8 = IsV8U8(stream);
+		if (isv8u8 == true)
+			return new V8U8Image(stream);
+		else if (isv8u8 == false)
+			return new ResILImage(stream);
+			
+		throw new InvalidDataetc();
+	}
+	
+	public static ResILImage Create(byte[] imageData)
+	{
+		using (MemoryTributary stream = new MemoryTributary(imageData))
+			return Create(stream);
+	}
+	
+	static bool? IsV8U8(MemoryTributary stream)
+	{
+		stream.Seek(0, SeekOrigin.Begin);
+		using (BinaryReader r = new BinaryReader(stream, true)
+		{
+			int dwMagic = r.ReadInt32();
+	        if (dwMagic != 0x20534444)
+	            return null;
+	
+	        DDS_HEADER header = new DDS_HEADER();
+	        Read_DDS_HEADER(header, r);
+	
+	        if (((header.ddspf.dwFlags & 0x00000004) != 0) && (header.ddspf.dwFourCC == 0x30315844 /*DX10*/))
+	        {
+	            throw new Exception("DX10 not supported yet!");
+	        }
+			
+			return header.ddspf.dwFourCC == V8U8;
+		}
+	}
+	#endregion Creation
 }
